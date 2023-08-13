7bef15f7b95e97be47a32a11f0318a5a1ce827c4
to-yuki <to-yuki@jtp.co.jp>
2023-08-14 01:03:09 +0900

    20230814

---
+++ cf/labs/cdk/cloudtraining_cf_cdk_ec2_stack.py
@@ -0,0 +1,56 @@
+from aws_cdk import (
+    # Duration,
+    aws_ec2 as ec2,
+    aws_iam as iam,
+    Stack,
+    # aws_sqs as sqs,
+)
+from constructs import Construct
+
+class CloudtrainingCfCdkEc2Stack(Stack):
+
+    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+        # IDから既存のVPCを取得する
+        vpc=ec2.Vpc.from_lookup(self, 'vpcId', vpc_id='vpc-xxxxxxxxxxxxxxxx')
+        
+        # Create SecurityGroup
+        security_group = ec2.SecurityGroup(
+            self,
+            id="cloudtrainingXX-cdk-ec2-sg",     # セキュリティグループID
+            vpc=vpc,
+            allow_all_outbound=True,   # 外部接続の許可
+            security_group_name="cloudtrainingXX-cdk-ec2-sg"   # セキュリティグループ名
+        )
+        # Add Ingress Rule
+        security_group.add_ingress_rule(
+            peer=ec2.Peer.ipv4("10.0.0.0/16"),   # アクセス可能IP
+            connection=ec2.Port.tcp(22),    # 22番ポートの許可
+            description="allow ssh access"
+        )
+        
+        # Instance Role and SSM Managed Policy 
+        role = iam.Role(self, "InstanceSSM",assumed_by=iam.ServicePrincipal("ec2.amazonaws.com"))
+        role.add_managed_policy(iam.ManagedPolicy.from_aws_managed_policy_name("AmazonSSMManagedInstanceCore"))
+        
+        # Set the AMI from Amazon Linux2 
+        machine_image=ec2.MachineImage.generic_linux({"ap-northeast-3": "ami-xxxxxxxxxxxxxxxx"})
+        
+        ec2_instance = ec2.Instance(
+            self,
+            id="cloudtrainingXX-cdk-ec2-instance", # EC2のインスタンスID
+            role=role,
+            instance_type=ec2.InstanceType.of(
+                ec2.InstanceClass.BURSTABLE2,       # EC2のインスタンスタイプ
+                ec2.InstanceSize.MICRO            # EC2のインスタンスタイプのサイズ
+            ),
+            machine_image=machine_image, # Amazon Linux最新.Ver
+            vpc=vpc,
+            vpc_subnets=ec2.SubnetSelection(
+                subnet_type=ec2.SubnetType.PRIVATE_WITH_EGRESS  # EC2のサブネット指定
+            ),
+            instance_name="cloudtrainingXX-cdk-ec2-instance",       # EC2の名前指定
+            security_group=security_group,            # セキュリティグループ指定
+            key_name= "cdk-app"                     # SSHキーペア名指定
+        )
+        
---
+++ cf/labs/cdk/cloudtraining_cf_cdk_vpc_stack.py
@@ -0,0 +1,34 @@
+from aws_cdk import (
+    # Duration,
+    Stack,
+    aws_iam as iam,
+    aws_ec2 as ec2,
+)
+
+from constructs import Construct
+
+class CloudtrainingCfCdkVpcStack(Stack):
+
+    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+        # Create VPC
+        vpc = ec2.Vpc(
+            self,
+            id="cloudtrainingXX-cdk-vpc",
+            ip_addresses=ec2.IpAddresses.cidr("10.x.0.0/16"), # NatGateway
+            max_azs=x, 
+            nat_gateways=x, # NatGateway作成(0:なし1:あり)
+            # Create Private Subnet
+            subnet_configuration=[     # Subネットの構成(Public2個)
+                ec2.SubnetConfiguration(
+                    name="cloudtrainingXX-public-subnet", # PublicSubネットに追記される名前
+                    subnet_type=ec2.SubnetType.PUBLIC,
+                    cidr_mask=24
+                ),
+                ec2.SubnetConfiguration(
+                    name="cloudtrainingXX-private-subnet", # PrivateSubネットに追記される名前
+                    subnet_type=ec2.SubnetType.PRIVATE_WITH_EGRESS,
+                    cidr_mask=24
+                )
+            ]
+        )
